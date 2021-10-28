c6edc0ea6d3d038ab61ea59adf9b50b9566a7848
Chris-M-Bennett <cmbennett1@hotmail.com>
2021-10-28 19:56:33 +0100

    Implemented ES changing to all HUD buttons

--- Assets/Scripts/ChangeOpponentES.cs
+++ Assets/Scripts/ChangeOpponentES.cs
@@ -6,25 +6,25 @@ using UnityEngine.UI;
 
 public class ChangeOpponentES : MonoBehaviour
 {
-    public static DebateHUDScript opponentHUD;
-
-
-    // ReSharper disable Unity.PerformanceAnalysis
-    public static void OpponentESChange(GameObject opponent, int damage, Slider esMeter){
+    public static void OpponentESChange(int startES, Button choiceButton, GameObject choicePanel, GameObject opponent, int damage, Slider esMeter){
         if (opponent.GetComponent<DebateValuesScript>())
         {
             DebateValuesScript opponentValues = opponent.GetComponent<DebateValuesScript>();
-            opponentValues.currentES += damage;
-            if (opponentValues.currentES > opponentValues.maxES)
-            {
-                opponentValues.currentES = opponentValues.maxES;
+            int opponentES = opponentValues.currentES;
+            if(opponentES == startES){
+                opponentValues.currentES += damage;
+                if (opponentES > opponentValues.maxES)
+                {
+                    opponentES = opponentValues.maxES;
+                }
+                else if (opponentES < 0)
+                {
+                    opponentES = 0;
+                }
+                esMeter.value = opponentES;
+                choicePanel.SetActive(false);
+                Debug.Log("sfd");
             }
-            else if (opponentValues.currentES < 0)
-            {
-                opponentValues.currentES = 0;
-            }
-
-            esMeter.value = opponentValues.currentES;
-        }else{Debug.LogError("Supplied Game Objects is not an opponent");}
+        }else{Debug.LogError("Supplied Game Object is not an opponent");}
     }
 }
--- Assets/Scripts/PlayerDebateActionsScript.cs
+++ Assets/Scripts/PlayerDebateActionsScript.cs
@@ -1,204 +1,127 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public enum States { Neutral, Persuade, Argue, Act, Items, 
-    OpPersuade, Emotions, OnTopic, Ethos,
-    Happy, Angry, Sad, Anxious}
-public class PlayerDebateActionsScript : MonoBehaviour
+    Emotions, Happy, Angry, Sad, Anxious}
+public partial class PlayerDebateActionsScript : MonoBehaviour
 {
-    public const string CHECK = "Check";
-    public const string GIFT = "Gift";
-    public const string RUN = "Run";
-    public const string OVERLOADPACIFY = "Overload/Pacify";
-    public const string PERSUADEVARIED = "Varying";
-    public const string THANK = "Thank";
-    public const string ONTOPIC = "On Topic";
-    public const string ETHOS = "Ethos";
-    public const string ARGUEVARIED = "Varying";
-    public const string PROVOKE = "Provoke";
-    private const string CONFUSE = "Confuse";
-    public const string PATHOS = "Pathos";
-
     public States state = States.Neutral;
-    [SerializeField] private GameObject choicePanel;
-    [SerializeField] private GameObject choice1Button;
-    [SerializeField] private GameObject choice2Button;
-    [SerializeField] private GameObject choice3Button;
-    [SerializeField] private GameObject choice4Button;
+    public GameObject choicePanel;
+    [SerializeField] private GameObject choice1;
+    [SerializeField] private GameObject choice2;
+    [SerializeField] private GameObject choice3;
+    [SerializeField] private GameObject choice4;
     [SerializeField] private Slider opponentESSlider;
     private GameObject _opponentGO;
+    private Button _choice1Button;
+    private Button _choice2Button;
+    private Button _choice3Button;
+    private Button _choice4Button;
     private Text _choice1Text;
     private Text _choice2Text;
     private Text _choice3Text;
     private Text _choice4Text;
     private bool _isArguing;
-    private bool _clicked = false;
-    private int _persuade1Change = 2;
 
     void Start()
     {
-        _choice1Text = choice1Button.transform.GetComponentInChildren<Text>();
-        _choice2Text = choice2Button.transform.GetComponentInChildren<Text>();
-        _choice3Text = choice3Button.transform.GetComponentInChildren<Text>();
-        _choice4Text = choice4Button.transform.GetComponentInChildren<Text>();
+        _choice1Button = choice1.GetComponent<Button>();
+        _choice2Button = choice2.GetComponent<Button>();
+        _choice3Button = choice3.GetComponent<Button>();
+        _choice4Button = choice4.GetComponent<Button>();
+        _choice1Text = choice1.transform.GetComponentInChildren<Text>();
+        _choice2Text = choice2.transform.GetComponentInChildren<Text>();
+        _choice3Text = choice3.transform.GetComponentInChildren<Text>();
+        _choice4Text = choice4.transform.GetComponentInChildren<Text>();
         _opponentGO = GetComponentInParent<DebateSystemScript>().opponentGO;
-        // _panelsArray = new GameObject[4];
-        // _panelsArray[0] = persuadePanel;
-        // _panelsArray[1] = arguePanel;
-        // _panelsArray[2] = actPanel;
-        //_panelsArray[3] = itemsPanel;
-        //Ensures no panels are showed when scene is started
         //ClosePanels();
     }
 
 
-    void SetButtonUnlock(GameObject button, bool state)
+    void SetButtonUnlock(bool state)
     {
-        button.GetComponent<Button>().enabled = state;
-        button.GetComponent<Image>().color = state ? Color.white : Color.gray;
+        _choice4Button.enabled = state;
+        choice4.GetComponent<Image>().color = state ? Color.white : Color.gray;
     }
 
-    void SetButtonOnClick(GameObject button, int change)
-    {
-        button.GetComponent<Button>().onClick.AddListener(()=>ChangeOpponentES.OpponentESChange(_opponentGO, change, opponentESSlider));
-        _clicked = true;
-    }
-    
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         switch(state){
             case States.Act:
-                ChangeTextChoices(CHECK, GIFT, RUN, OVERLOADPACIFY);
-                SetButtonUnlock(choice4Button, true);
+                ChangeState(ACT_1, States.Neutral, ACT_2, States.Neutral, ACT_3,
+                    States.Neutral, ACT_4, States.Neutral);
+                SetButtonUnlock(false);
                 break;
             case States.Persuade:
                 _isArguing = false;
-                /*_choice1Text.text = "Varying";
-                //choice1Button.GetComponent<Button>().onClick.AddListener(OpponentUniquePersuade);
-                _choice2Text.text = "Thank";
-                choice2Button.GetComponent<Button>().onClick.AddListener(delegate{PanelButton(States.Thank);});
-                _choice3Text.text = "On Topic";
-                //choice3Button.GetComponent<Button>().onClick.AddListener(OnTopic);
-                _choice4Text.text = "Ethos";
-                //choice4Button.GetComponent<Button>().onClick.AddListener(Ethos);*/
-                ChangeState(PERSUADEVARIED,States.Neutral, THANK, States.Emotions,  ONTOPIC,  
-                    States.Neutral, ETHOS,States.Neutral);
-                SetButtonUnlock(choice4Button, false);
+                ChangeState(PER_1_VARIED,States.Emotions, PER_2, States.Emotions,  PER_3,  
+                    States.Emotions, PER_4,States.Emotions);
+                SetButtonUnlock(false);
                 // choice4Button.GetComponent<Button>().enabled = false;
                 // choice4Button.GetComponent<Image>().color = Color.gray;
                 break;
             
             case States.Argue:
                 _isArguing = true;
-                ChangeState(ARGUEVARIED,States.Neutral, PROVOKE, States.Emotions,  CONFUSE,  
-                    States.Neutral, PATHOS,States.Neutral);
-                SetButtonUnlock(choice4Button, false);
+                ChangeState(ARG_1_VARIED,States.Emotions, ARG_2, States.Emotions,  ARG_3,  
+                    States.Emotions, ARG_4,States.Emotions);
+                SetButtonUnlock(false);
 
                 break;
             
             case States.Emotions:
-                ChangeState("Happy",States.Happy, "Angry", States.Angry,  "Sad",  
-                    States.Sad, "Anxious",States.Anxious);
+                ChangeState(HAPPY,States.Happy, ANGRY, States.Angry,  "Sad",  
+                    States.Sad, ANXIOUS,States.Anxious);
+                SetButtonUnlock(true);
                 break;
             case States.Happy:
-                if (!_clicked)
+                if (_isArguing)
                 {
-                    if (_isArguing)
-                    {
-                        ChangeTextChoices("Mock", "Tease", "Laugh", "");
-                    }
-                    else
-                    {
-                        ChangeTextChoices("Pet", "Dance", "Motivate", "");
-                        SetButtonOnClick(choice1Button, _persuade1Change);
-
-                    }
+                    ChangeAction(HAPPY_ARG_1, ARG_CHANGE_1, HAPPY_ARG_2, ARG_CHANGE_2, HAPPY_ARG_3,
+                        ARG_CHANGE_3, EMPTY, ARG_CHANGE_4);
+                }
+                else
+                {
+                    ChangeAction(HAPPY_PER_1, PER_CHANGE_1, HAPPY_PER_2, PER_CHANGE_2,
+                        HAPPY_PER_3, PER_CHANGE_3, EMPTY, PER_CHANGE_4);
                 }
 
                 break;
             case States.Angry:
                 if(_isArguing){
-                    _choice1Text.text = "Intimidate";
-                    //choice1Button.GetComponent<Button>().onClick.AddListener(OpponentUniqueArgue);
-                    _choice2Text.text = "Rant";
-                    //choice2Button.GetComponent<Button>().onClick.AddListener(Provoke);
-                    _choice3Text.text = "Shout";
-                    //choice3Button.GetComponent<Button>().onClick.AddListener();
-                    choice4Button.SetActive(false);
+                        ChangeAction(ANGRY_ARG_1, ARG_CHANGE_1, ANGRY_ARG_2, ARG_CHANGE_2, 
+                            ANGRY_ARG_3, ARG_CHANGE_3, EMPTY, ARG_CHANGE_4);
                 }else
                 {
-                    _choice1Text.text = "Assert";
-                    //choice1Button.GetComponent<Button>().onClick.AddListener(OpponentUniqueArgue);
-                    _choice2Text.text = "Work out";
-                    //choice2Button.GetComponent<Button>().onClick.AddListener(Provoke);
-                    _choice3Text.text = "Timeout";
-                    //choice3Button.GetComponent<Button>().onClick.AddListener();
-                    choice4Button.SetActive(false);
+                    ChangeAction(ANGRY_PER_1, PER_CHANGE_1, ANGRY_PER_2, PER_CHANGE_2, 
+                        ANGRY_PER_3, PER_CHANGE_3, EMPTY, PER_CHANGE_4);
                 }
                 break;
             case States.Sad:
                 if(_isArguing){
-                    _choice1Text.text = "Guilt";
-                    //choice1Button.GetComponent<Button>().onClick.AddListener(OpponentUniqueArgue);
-                    _choice2Text.text = "Crocodile Tears";
-                    //choice2Button.GetComponent<Button>().onClick.AddListener(Provoke);
-                    _choice3Text.text = "Be Numb";
-                    //choice3Button.GetComponent<Button>().onClick.AddListener();
-                    choice4Button.SetActive(false);
+                        ChangeAction(SAD_ARG_1, ARG_CHANGE_1,SAD_ARG_2, ARG_CHANGE_2,
+                            SAD_ARG_3, ARG_CHANGE_3, EMPTY, ARG_CHANGE_4);
                 }else
                 {
-                    _choice1Text.text = "Cry With";
-                    //choice1Button.GetComponent<Button>().onClick.AddListener(OpponentUniqueArgue);
-                    _choice2Text.text = "Write Poetry";
-                    //choice2Button.GetComponent<Button>().onClick.AddListener(Provoke);
-                    _choice3Text.text = "Comfort";
-                    //choice3Button.GetComponent<Button>().onClick.AddListener();
-                    choice4Button.SetActive(false);
+                    ChangeAction(SAD_PER_1, PER_CHANGE_1, SAD_PER_2, PER_CHANGE_2,
+                        SAD_PER_3,PER_CHANGE_3, EMPTY, PER_CHANGE_4);
                 }
                 break;
             case States.Anxious:
                 if(_isArguing){
-                    _choice1Text.text = "Freak Out";
-                    //choice1Button.GetComponent<Button>().onClick.AddListener(OpponentUniqueArgue);
-                    _choice2Text.text = "Overthink";
-                    //choice2Button.GetComponent<Button>().onClick.AddListener(Provoke);
-                    _choice3Text.text = "Induce Dread";
-                    //choice3Button.GetComponent<Button>().onClick.AddListener();
-                    choice4Button.SetActive(false);
+                        ChangeAction(ANXIOUS_ARG_1, ARG_CHANGE_1,ANXIOUS_ARG_2, ARG_CHANGE_2,
+                            ANXIOUS_ARG_3, ARG_CHANGE_3, EMPTY, ARG_CHANGE_4);
                 }else
                 {
-                    _choice1Text.text = "Ask About";
-                    //choice1Button.GetComponent<Button>().onClick.AddListener(OpponentUniqueArgue);
-                    _choice2Text.text = "Give Idea";
-                    //choice2Button.GetComponent<Button>().onClick.AddListener(Provoke);
-                    _choice3Text.text = "Deep Breaths";
-                    //choice3Button.GetComponent<Button>().onClick.AddListener();
-                    choice4Button.SetActive(false);
+                    ChangeAction(ANXIOUS_PER_1, PER_CHANGE_1, ANXIOUS_PER_2, PER_CHANGE_2,
+                        ANXIOUS_PER_3,PER_CHANGE_3, EMPTY, PER_CHANGE_4);
                 }
                 break;
         }
     }
 
-    private void ChangeTextChoices(string c1, string c2, string c3, string c4)
-    {
 
-        choice1Button.SetActive(c1 != "");
-        choice2Button.SetActive(c2 != "");
-        choice3Button.SetActive(c3 != "");
-        choice4Button.SetActive(c4 != "");
-
-        _choice1Text.text = c1;
-        _choice2Text.text = c2;
-        _choice3Text.text = c3;
-        _choice4Text.text = c4;
-        
-    }
-
-    public void PanelButton(States changeState){
+    private void PanelButton(States changeState){
         choicePanel.SetActive(true);
         state = changeState;
     }
@@ -219,27 +142,43 @@ public class PlayerDebateActionsScript : MonoBehaviour
         //PanelButton( States.Items);
     }
     
-    void Change(){}
+    void ChangeTexts(string text1, string text2, string text3, string text4){
+       //Removes listeners previous added to buttons
+        _choice1Button.onClick.RemoveAllListeners();
+        _choice2Button.onClick.RemoveAllListeners();
+        _choice3Button.onClick.RemoveAllListeners();
+        _choice4Button.onClick.RemoveAllListeners();
+        
+        //Checks text for buttons to see if they should be active
+        choice1.SetActive(text1 != "");
+        choice2.SetActive(text2 != "");
+        choice3.SetActive(text3 != "");
+        choice4.SetActive(text4 != "");
+        
+        _choice1Text.text = text1;
+        _choice2Text.text = text2;
+        _choice3Text.text = text3;
+        _choice4Text.text = text4;
+    }
+    
     void ChangeState(string text1, States state1, string text2, States state2,
         string text3, States state3, string text4, States state4)
     {
-        _clicked = false;
-        _choice1Text.text = text1;
-        choice1Button.GetComponent<Button>().onClick.AddListener(()=>PanelButton(state1));
-        _choice2Text.text = text2;
-        choice2Button.GetComponent<Button>().onClick.AddListener(()=>PanelButton(state2));
-        _choice3Text.text = text3;
-        choice3Button.GetComponent<Button>().onClick.AddListener(()=>PanelButton(state3));
-        _choice4Text.text = text4;
-        choice4Button.GetComponent<Button>().onClick.AddListener(()=>PanelButton(state4));
+        ChangeTexts(text1, text2, text3, text4);
+        _choice1Button.onClick.AddListener(()=>PanelButton(state1));
+        _choice2Button.onClick.AddListener(()=>PanelButton(state2));
+        _choice3Button.onClick.AddListener(()=>PanelButton(state3));
+        _choice4Button.onClick.AddListener(()=>PanelButton(state4));
     }
-
-    /// <summary>
-    /// Hides all panels in the panels array
-    /// </summary>
-    /*void ClosePanels(){
-        foreach (var i in _panelsArray)
-        {
-            i.SetActive(false);
-        }*/
+    
+    void ChangeAction(string text1, int change1,string text2, int change2,
+        string text3, int change3, string text4, int change4){
+        ChangeTexts(text1, text2, text3, text4);
+        //Gets opponent's ES value before change for comparison
+        int startES = _opponentGO.GetComponent<DebateValuesScript>().currentES;
+        _choice1Button.onClick.AddListener(()=>ChangeOpponentES.OpponentESChange(startES, _choice1Button, choicePanel, _opponentGO, change1, opponentESSlider));
+        _choice2Button.onClick.AddListener(()=>ChangeOpponentES.OpponentESChange(startES, _choice2Button, choicePanel,_opponentGO, change2, opponentESSlider));
+        _choice3Button.onClick.AddListener(()=>ChangeOpponentES.OpponentESChange(startES, _choice3Button, choicePanel,_opponentGO, change3, opponentESSlider));
+        _choice4Button.onClick.AddListener(()=>ChangeOpponentES.OpponentESChange(startES, _choice4Button, choicePanel,_opponentGO, change4, opponentESSlider));
+    }
 }
